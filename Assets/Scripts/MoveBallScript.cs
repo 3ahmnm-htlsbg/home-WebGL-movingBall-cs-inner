d4946750f443c21c0003ac5b8b219f222626172c
Eva Innerlohninger <evainnerlohninger@einner.local>
2020-01-13 18:25:07 +0100

    write script

---
+++ Assets/Scripts/MoveBallScript.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+// using UnityEngine.UI;
+
+public class MoveBallScript : MonoBehaviour
+{
+[SerializeField]  private GameObject[] ballPositions;
+public GameObject ball;
+int RandomPos;
+int PosBefore;
+
+public void SetBallRandom() {
+
+ball.transform.position = ballPositions[RandomNumber()].transform.position;
+}
+int RandomNumber() {
+RandomPos = Random.Range(0,4);
+while(RandomPos == PosBefore) {
+RandomPos = Random.Range(0,4);
+}
+PosBefore = RandomPos;
+return RandomPos;
+}
+
+
+}
+
+    
+
